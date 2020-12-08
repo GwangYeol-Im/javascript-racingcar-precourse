08082139a39193619139162bc20a9ccfd1eee646
임광열 <mkitigy@gmail.com>
2020-12-08 19:29:21 +0900

    chores: editor configuration

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: ['prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-new': 'off',
+    'class-methods-use-this': 'off',
+    'import/no-named-as-default': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-alert': 'off',
+    'max-depth': ['error', 2],
+    'max-lines-per-function': ['error', 15],
+  },
+};
